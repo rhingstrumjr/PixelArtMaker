13b516d95ffaeab496f4bf9f0310eeaf7bfa1d7b
Robert Hingstrum <rhingstrumjr@gmail.com>
2019-01-23 15:11:44 -0600

    Able to collect height and width values

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // return height and width values
 function heightAndWidth() {
-  x = document.getElementById('width');
-  y = document.getElementById('height');
+  x = document.getElementById('width').value;
+  y = document.getElementById('height').value;
   console.log(x, y);
 }
